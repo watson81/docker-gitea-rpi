c2cba82b65dd1455cfa00e4734fdb90dff18d58f
Patrick Watson <watson81@users.noreply.github.com>
2022-03-25 22:30:45 -0400

    Add environment-to-ini & /usr/local/bin/gitea shim
    Fixes https://github.com/watson81/docker-gitea-rpi/issues/2

--- build.sh
+++ build.sh
@@ -15,7 +15,10 @@ if [ "$(docker images -q ${REPO}:${VERSION} 2> /dev/null)" != "" ]; then
   exit 2
 fi
 
-set -x
 docker build --pull --tag "${REPO}:${VERSION}" --build-arg "VERSION=${VERSION}" --file "Dockerfile.$(uname -m)" .
-echo "[INFO] Build Complete. Validate version:"
+
+printf "\n[\e[1;34mINFO\e[0m] Build Complete. \e[1;36mValidate version=%s\e[0m:\n" "${VERSION}"
 docker run --rm "${REPO}:${VERSION}" /app/gitea/gitea --version
+
+printf "\n[\e[1;34mINFO\e[0m] Build Complete. \e[1;36mValidate startup & HTTP Port 9999\e[0m:\n"
+docker run --rm -it -e GITEA__SERVER__HTTP_PORT=9999 "${REPO}:${VERSION}"
